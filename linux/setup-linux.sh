ba9b60b809a7f6370e3a7ab1519c2f4d7ddb59a0
Khai Weng Au Yeong <aykhaiweng@gmail.com>
2018-03-07 18:30:50 +0800

    Added installation of pyenv virtualenv

--- linux/setup-linux.sh
+++ linux/setup-linux.sh
@@ -1,67 +1,70 @@
-# !/bin/bash
+#!/bin/bash
 
 ###############################################################################
 # Linux Setup
 ###############################################################################
 
-function _echo() {
+_echo() {
 	echo "[aykhaiweng says][Linux] - $1"
 }
 
-# prompt for sudo
-sudo -v
+main() {
+	# prompt for sudo
+	sudo -v
 
-# Install essentials
-_echo "Installing essentials."
-_echo
-echo "@================================================="
-echo "| khai.IO                                        |"
-echo "|                                                |"
-echo "| The following programs will be                 |"
-echo "| installed:-                                    |"
-echo "|    1. python3              8. nginx            |"
-echo "|    2. python3-pip          9. gunicorn         |"
-echo "|    3. python-dev           10. git             |"
-echo "|    4. libpq-dev            11. memcached       |"
-echo "|    5. postgresql           12. system-services |"
-echo "|    6. mysql                13. nodejs-legacy   |"
-echo "|    7. libjpeg8-dev         14. npm             |"
-echo "|                                                |"
-echo "|                    And loads of other goodies! |"
-echo "@================================================="
-echo
+	# Install essentials
+	_echo "Installing essentials."
+	_echo
+	echo "@================================================="
+	echo "| khai.IO                                        |"
+	echo "|                                                |"
+	echo "| The following programs will be                 |"
+	echo "| installed:-                                    |"
+	echo "|    1. python3              8. nginx            |"
+	echo "|    2. python3-pip          9. gunicorn         |"
+	echo "|    3. python-dev           10. git             |"
+	echo "|    4. libpq-dev            11. memcached       |"
+	echo "|    5. postgresql           12. system-services |"
+	echo "|    6. mysql                13. nodejs-legacy   |"
+	echo "|    7. libjpeg8-dev         14. npm             |"
+	echo "|                                                |"
+	echo "|                    And loads of other goodies! |"
+	echo "@================================================="
+	echo
 
-sudo apt-get update
-sudo apt-get install build-essential curl file git -y
-sudo apt-get install vim systemd -y
-sudo apt-get install git python3-pip python-dev python-setuptools -y
-sudo apt-get install nginx gunicorn memcached -y
-sudo apt-get install libpq-dev postgresql postgresql-contrib gettext -y
-sudo apt-get install nodejs-legacy npm libjpeg8-dev -y
-sudo apt-get install postfix mailutils libsasl2-2 ca-certificates libsasl2-modules -y
-sudo apt-get install libffi-dev libssl-dev libxml2-dev libxslt1-dev -y
-sudo apt-get install letsencrypt -y
-sudo apt-get install mysql-server libmysqlclient-dev -y
+	sudo apt-get update
+	sudo apt-get install build-essential curl file git -y
+	sudo apt-get install vim systemd -y
+	sudo apt-get install git python3-pip python-dev python-setuptools -y
+	sudo apt-get install nginx gunicorn memcached -y
+	sudo apt-get install libpq-dev postgresql postgresql-contrib gettext -y
+	sudo apt-get install nodejs-legacy npm libjpeg8-dev -y
+	sudo apt-get install postfix mailutils libsasl2-2 ca-certificates libsasl2-modules -y
+	sudo apt-get install libffi-dev libssl-dev libxml2-dev libxslt1-dev -y
+	sudo apt-get install letsencrypt -y
+	sudo apt-get install mysql-server libmysqlclient-dev -y
 
-# terminal essentials
-sudo apt-get install zsh tmux xclip -y
+	# terminal essentials
+	sudo apt-get install zsh tmux xclip -y
 
-if ! [ $KURA_FTS ] ; then
-    _echo "Setting up locales"
-    sudo locale-gen en_US.UTF-8
-    sudo timedatectl set-timezone Asia/Kuala_Lumpur
-fi
+	if ! [ $KURA_FTS ] ; then
+		_echo "Setting up locales"
+		sudo locale-gen en_US.UTF-8
+		sudo timedatectl set-timezone Asia/Kuala_Lumpur
+	fi
 
-# sudo -u postgres psql -c "create user $USER superuser password 'oceannexus129';" postgres
+	# sudo -u postgres psql -c "create user $USER superuser password 'oceannexus129';" postgres
 
-if ! [ -f ~/.ssh/id_rsa.pub ]; then
-    _echo "Generating SSH Keys"
-    ssh-keygen
-    echo
-fi
+	if ! [ -f ~/.ssh/id_rsa.pub ]; then
+		_echo "Generating SSH Keys"
+		ssh-keygen
+		echo
+	fi
 
-# Specifically for servers, this is a requirement
-pip install virtualenv
+	# Specifically for servers, this is a requirement
+	pip install virtualenv
 
-# installing pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	# installing pyenv
+	git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+	git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
+}
--- setup.sh
+++ setup.sh
@@ -1,69 +1,74 @@
-# !/bin/bash
+#!/bin/bash
 
-function _echo() {
+_echo() {
 	echo "[aykhaiweng says] - $1"
 }
 
-# prompt for sudo
-_echo "Before I start installing anything, I'm going to need your password for sudo access."
-sudo -v
+main() {
+	# prompt for sudo
+	_echo "Before I start installing anything, I'm going to need your password for sudo access."
+	sudo -v
 
-# declare variable for os type
-uname=$(uname)
+	# declare variable for os type
+	uname=$(uname)
 
-_echo "$uname detected."
+	_echo "$uname detected."
 
-if [[ $uname == 'Darwin' ]]; then
-	_echo "Setting up for Darwin platform."
-	source osx/setup-osx.sh
-elif [[ $uname == 'Linux' ]]; then
-	_echo "Setting up for Linux platform."
-	source linux/setup-linux.sh
-else
-	_echo "Your platform is not supported yet."
-fi
-
-
-# installing Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# updating pip and it's prerequisites
-_echo "Upgrading pip and installing prerequisites."
-sudo pip3 install --upgrade pip
-sudo pip3 install django django-debug-toolbar Pillow psycopg2 ipython coverage
-echo
+	if [[ $uname == 'Darwin' ]]; then
+		_echo "Setting up for Darwin platform."
+		source osx/setup-osx.sh
+	elif [[ $uname == 'Linux' ]]; then
+		_echo "Setting up for Linux platform."
+		source linux/setup-linux.sh
+	else
+		_echo "Your platform is not supported yet."
+	fi
 
 
-# prepare to symlink the files
-_echo "Now preparing to symlink files."
-python3 symlink.py
+	# installing Vundle
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# run the aliases file to get the pyenv out
-source $HOME/.aliases
+	# updating pip and it's prerequisites
+	_echo "Upgrading pip and installing prerequisites."
+	sudo pip3 install --upgrade pip
+	sudo pip3 install django django-debug-toolbar Pillow psycopg2 ipython coverage
+	echo
 
-# set zsh as default
-if [[ $SHELL != '/bin/zsh' ]]; then
-	_echo "Current shell is `$SHELL`, changing to `/bin/zsh`"
-	chsh -s $(which zsh)
-fi
 
-# now we install .oh-my-zsh
-yes '' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	# prepare to symlink the files
+	_echo "Now preparing to symlink files."
+	python3 symlink.py
 
-# installing pyenv 3.6.4 and 2.7.13
-yes '' | pyenv install 3.6.4
-yes '' | pyenv install 2.7.13
+	# run the aliases file to get the pyenv out
+	source $HOME/.aliases
 
-# create an id_rsa file if it doesn't already exist
-if [ ! -f $HOME/.ssh/id_rsa ]; then
-	ssh-keygen -f id_rsa -t rsa -N ''
-	_echo "A ssh-key with the default name (~/.ssh/id_rsa) has been created for you!"
-	_echo "Here is the key:" 
-	echo "$(cat $HOME/.ssh/id_rsa.pub)"
-fi
+	# set zsh as default
+	if [[ $SHELL != '/bin/zsh' ]]; then
+		_echo "Current shell is `$SHELL`, changing to `/bin/zsh`"
+		chsh -s $(which zsh)
+	fi
 
-# finalize
-_echo "Reloading ~/.zshrc."
-exec "$SHELL"
-source ~/.zshrc
-_echo ".dotfiles setup complete! Enjoy living in a world of monogamy."
+	# now we install .oh-my-zsh
+	yes '' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+	# installing pyenv 3.6.4 and 2.7.13
+	yes '' | pyenv install 3.6.4
+	yes '' | pyenv install 2.7.13
+
+	# create an id_rsa file if it doesn't already exist
+	if [ ! -f $HOME/.ssh/id_rsa ]; then
+		ssh-keygen -f id_rsa -t rsa -N ''
+		_echo "A ssh-key with the default name (~/.ssh/id_rsa) has been created for you!"
+		_echo "Here is the key:" 
+		echo "$(cat $HOME/.ssh/id_rsa.pub)"
+	fi
+
+	# finalize
+	_echo "Reloading ~/.zshrc."
+	exec "$SHELL"
+	source ~/.zshrc
+	_echo ".dotfiles setup complete! Enjoy living in a world of monogamy."
+}
+
+# invoke main
+main
