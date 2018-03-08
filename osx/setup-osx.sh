cf832dfdd308b9300dd3dca2c85218ec21000c8b
Khai Weng Au Yeong <aykhaiweng@gmail.com>
2018-03-09 01:39:07 +0800

    Added invocation for osx installation

--- osx/setup-osx.sh
+++ osx/setup-osx.sh
@@ -11,10 +11,18 @@ function _echo() {
 # prompt for sudo
 sudo -v
 
-# Install brew
-_echo "Installing Brew."
-yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# install the brew bundle
-brew bundle --file=brew/Brewfile
+main() {
+	# Install brew
+	_echo "Installing Brew."
+	yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+	# install the brew bundle
+	brew bundle --file=brew/Brewfile
+
+	# add the oceanic item colors
+	source $HOME/.dotfiles/iterm2/OceanicNext.itermcolors
+}
+
+# invoke main
+main
