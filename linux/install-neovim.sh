49a8dd9dca253f076596352c8076c66aec568a79
Khai Weng Au Yeong <aykhaiweng@gmail.com>
2018-04-10 07:55:06 +0800

    Fixed installation for neovim

--- linux/install-neovim.sh
+++ linux/install-neovim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir /opt/nvim
-curl -o /opt/nvim/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+curl -LO -o /opt/nvim/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod a+x /opt/nvim/nvim.appimage
 ln -s /opt/nvim/nvim.appimage /bin/nvim
