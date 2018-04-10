1f79c7c288fd9a0b9b3fe45f97f9d1a76f97709a
Khai Weng Au Yeong <aykhaiweng@gmail.com>
2018-04-10 08:10:31 +0800

    Changed the way the neovim is moved to the opt folderg

--- linux/install-neovim.sh
+++ linux/install-neovim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir /opt/nvim
-curl -LO -o /opt/nvim/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+mkdir /opt/nvim/
+mv nvim.appimage /opt/nvim/nvim.appimage
 chmod a+x /opt/nvim/nvim.appimage
 ln -s /opt/nvim/nvim.appimage /bin/nvim
