09b6174b574968e37b5621092205bd4575a936b5
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2018-06-13 14:14:19 +0800

    Updated rmate script

--- setup.sh
+++ setup.sh
@@ -139,7 +139,7 @@ main() {
 
 
     # Install Rmate
-    curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
+    sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
     sudo chmod +x /usr/local/bin/rmate
 
 
