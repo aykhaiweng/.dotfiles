faaa1cc0ba4577824881844317f4bfc438838cff
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 02:45:46 +0800

    Rearranged install orders

--- init/40_install_kr.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $(which kr) ]]; then
-	e_arrow "Krypton is already installed"
-else
-	e_success "Installing Krypton from https://krypt.co/kr"
-	curl https://krypt.co/kr | sh
-fi
