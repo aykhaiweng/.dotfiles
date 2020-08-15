b0db2ddd545f28029ba1f4150c46d39d54017e5b
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 17:35:47 +0800

    Better checking for installing kr

--- init/41_install_kr.sh
+++ init/41_install_kr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $(which kr) ]]; then
+if [[ -f "/usr/bin/kr" || -f "/usr/local/bin/kr" ]]; then
     e_arrow "Krypton is already installed"
 else
     e_success "Installing Krypton from https://krypt.co/kr"
--- source/11_krypton.sh
+++ source/11_krypton.sh
@@ -1,2 +1,6 @@
-# Added by Krypton
-export GPG_TTY=$(tty)
+#!/usr/bin/env bash
+
+if [[ -f "/usr/bin/kr" || -f "/usr/local/bin/kr" ]]; then
+    # Added by Krypton
+    export GPG_TTY=$(tty)
+fi
