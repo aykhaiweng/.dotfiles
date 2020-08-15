02f1faa1266d6292c67bdfd893ea9ab3a4458ae5
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 14:19:01 +0800

    Fixed typo

--- init/42_install_fzf.sh
+++ init/42_install_fzf.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -d '$HOME/.fzf' ]; then
+if [[ -d '$HOME/.fzf' ]]; then
+    e_arrow "FZF is already installed"
+else
     e_success "Installing FZF"
     git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
     $HOME/.fzf/install --all
-else
-    e_arrow "FZF is already installed"
 fi
