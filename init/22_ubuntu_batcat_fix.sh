87274130a23dccf581ed334dfa572b2707815b7a
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 17:13:40 +0800

    Added batcat fix

---
+++ init/22_ubuntu_batcat_fix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Ubuntu-only stuff. Abort if not Ubuntu.
+is_ubuntu || return 1
+
+# Create a bin folder for local users
+if [[ -f "/usr/bin/batcat" && ! -f "$HOME/.local/bin/bat" ]]; then
+    mkdir -p $HOME/.local/bin
+    ln -s /usr/bin/batcat $HOME/.local/bin/bat
+fi
--- source/13_batcat_ubuntu_fix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Ubuntu-only stuff. Abort if not Ubuntu.
-is_ubuntu || return 1
-
-
-alias bat="batcat"
