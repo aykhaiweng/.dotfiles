721dd7382a33e54da62e918efde19c3314797b0c
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 12:57:49 +0800

    Fixed typo

--- init/50_setup_vim.sh
+++ init/50_setup_vim.sh
@@ -28,10 +28,10 @@ if [[ ! -f $vim_plug_install_dir ]]; then
     curl -fLo $vim_plug_install_dir --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# Download Vim plugins.
-if [[ "$(type -p vim)" ]]; then
-    e_arrow "Running vim +PlugUpgrade +PlugUpdate +UpdateRemotePlugins"
-    nvim +PlugUpgrade +PlugUpdate +UpdateRemotePlugins +qall
+# Download nvim plugins.
+if [[ "$(type -p nvim)" ]]; then
+    e_arrow "Running nvim +PlugUpgrade +PlugUpdate +UpdateRemotePlugins"
+    nvim +PlugUpgrade +PlugUpdate +qall
 fi
 
 pyenv deactivate
--- source/10_oh-my-zsh.sh
+++ source/10_oh-my-zsh.sh
@@ -1,5 +1,5 @@
 # Run this only if zsh is running
-is_zsh() || return 1
+is_zsh || return 1
 
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
