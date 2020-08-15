0e234bdab5a004f220b377147e875e84594fc694
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 17:16:48 +0800

    Changed ls command to have --color=auto for ubuntu

--- init/22_ubuntu_batcat_fix.sh
+++ init/22_ubuntu_batcat_fix.sh
@@ -5,6 +5,7 @@ is_ubuntu || return 1
 
 # Create a bin folder for local users
 if [[ -f "/usr/bin/batcat" && ! -f "$HOME/.local/bin/bat" ]]; then
+    e_arrow "Creating link for batcat"
     mkdir -p $HOME/.local/bin
     ln -s /usr/bin/batcat $HOME/.local/bin/bat
 fi
--- source/12_ls.sh
+++ source/12_ls.sh
@@ -1,4 +1,4 @@
 is_osx && alias ls="ls -ACF"
-is_ubuntu && alias ls="ls -aCF"
+is_ubuntu && alias ls="ls -aCF --color=auto"
 
 alias ll="ls -l"
