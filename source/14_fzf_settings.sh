6ca5f4cc1459723a3234590643334346bb0ff336
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 17:38:17 +0800

    Added conditional pyenv

--- source/14_fzf_settings.sh
+++ source/14_fzf_settings.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export FZF_DEFAULT_OPTS='--height 50% --layout=reverse --border=rounded --inline-info'
+if [[ -d '$HOME/.fzf' ]]; then
+    export FZF_DEFAULT_OPTS='--height 50% --layout=reverse --border=rounded --inline-info'
 
-export FZF_DEFAULT_COMMAND="ag --ignore '*.pyc' --ignore '*/*.pyc' --ignore-dir '*pycache*' --ignore-dir '.tox' --hidden --skip-vcs-ignores -g ''"
-export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :100 {}"'
+    export FZF_DEFAULT_COMMAND="ag --ignore '*.pyc' --ignore '*/*.pyc' --ignore-dir '*pycache*' --ignore-dir '.tox' --hidden --skip-vcs-ignores -g ''"
+    export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :100 {}"'
+fi
--- source/30_pyenv.sh
+++ source/30_pyenv.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # Setting up pyenv for the Terminal
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+
+if [[ -d "$HOME/.pyenv" ]]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
