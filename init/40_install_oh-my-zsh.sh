2b02e472a8487178ad19ce8cdc90811c8c020e4d
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2020-08-15 12:38:25 +0800

    Removed submodules

--- init/40_install_oh-my-zsh.sh
+++ init/40_install_oh-my-zsh.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ -d "$HOME/.oh-my-zsh/" ]]; then
-	e_arrow "oh-my-zsh is already installed"
+    e_arrow "oh-my-zsh is already installed"
 else
-	e_success "Installing oh-my-zsh"
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    e_arrow "Setting KEEP_ZSHRC=yes"
+    export KEEP_ZSHRC=yes
+    e_success "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
