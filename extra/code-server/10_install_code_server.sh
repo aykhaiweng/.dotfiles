f3feeab773e20ccd1309f8186e26a848d299d777
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2021-04-21 02:22:18 +0800

    Added installing code-server from source [WIP]

--- extra/code-server/10_install_code_server.sh
+++ extra/code-server/10_install_code_server.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
-CODE_SERVER_VERSION="3.9.3"
-CODE_SERVER_DOWNLOAD_URL="https://github.com/cdr/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server-${CODE_SERVER_VERSION}-linux-amd64.tar.gz"
+CODE_SERVER_GIT="git@github.com:cdr/code-server.git"
+CODE_SERVER_GIT_DIR="/tmp/code-server-git/"
 
-echo "> Downloading from $CODE_SERVER_DOWNLOAD_URL"
-# Download the specified version of code-server
-wget "$CODE_SERVER_DOWNLOAD_URL" -O "/tmp/code-server-$CODE_SERVER_VERSION-linux-amd64.tar.gz"
-# Extra the code-server tar
-tar -xzf "/tmp/code-server-$CODE_SERVER_VERSION-linux-amd64.tar.gz" -C /tmp/
-# Check for an existing installation
-if [[ -d "/usr/lib/code-server/" ]]; then
-    # Remove if we can find it
-    echo "> Deleting existing version of code-server"
-    rm -r /usr/lib/code-server/
+echo "> Cloaning $CODE_SERVER_GIT"
+if [[ -d $CODE_SERVER_GIT_DIR ]]; then
+    git clone git@github.com:cdr/code-server.git $CODE_SERVER_GIT_DIR
+    cd $CODE_SERVER_GIT_DIR
+else
+    cd $CODE_SERVER_GIT_DIR
+    git fetch && git pull
 fi
-# Copy the recently downloaded version of code-server
-cp -r "/tmp/code-server-$CODE_SERVER_VERSION-linux-amd64" /usr/lib/code-server
 
-# Check if the symlink exists
-if [[ -f "/usr/bin/code-server" ]]; then
-    # Delete the symlink if it already exists
-    echo "> Deleting existing link of code-server"
-    rm /usr/bin/code-server
-fi
-sudo ln -s /usr/lib/code-server/bin/code-server /usr/bin/code-server
+
+yarn
+yarn task build:server:binary
+
+
+# Exit
+cd -
